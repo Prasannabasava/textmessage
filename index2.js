141ab36c039658c7e037ebea4e5709ce8676d43e
Prasannabasava <155958240+Prasannabasava@users.noreply.github.com>
2024-01-09 15:25:16 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,7 @@
+function displayMessage() {
+            
+    var enteredMessage = document.getElementById("textmessage").value;
+
+    
+    document.getElementById("displayedMessage").innerText = "Entered Message: " + enteredMessage;
+        }
